5502467ba78bc9c5a964af6b3f35231f3773e0a0
yuran777 <56239872+yuran777@users.noreply.github.com>
2023-12-10 23:36:12 +0900

    alias  deleted

--- 1_eks-workspace/eks.tf
+++ 1_eks-workspace/eks.tf
@@ -1,6 +1,6 @@
-data "aws_kms_key" "existing_kms_key" {
-  key_id = "arn:aws:kms:ap-northeast-2:970698899539:key/48ffc2e8-696a-485b-838f-03100c4f88e1"
-}
+# data "aws_kms_key" "existing_kms_key" {
+#   key_id = "arn:aws:kms:ap-northeast-2:970698899539:key/48ffc2e8-696a-485b-838f-03100c4f88e1"
+# }
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
@@ -12,13 +12,8 @@ module "eks" {
   cluster_additional_security_group_ids = [aws_security_group.security_group_eks_cluster.id]
   # kms_key_aliases = "kms_alias_test"
   # kms_key_aliases = ["alias/yuran_key_examples"]
-  kms_key_aliases = data.aws_kms_key.existing_kms_key.arn[0]
+  # kms_key_aliases = data.aws_kms_key.existing_kms_key.arn[0]
   
-  lifecycle {
-    ignore_changes = [
-      kms_key_aliases,
-    ]
-  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
